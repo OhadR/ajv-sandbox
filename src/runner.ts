34b08c6c24cb017a0f5f19f988e5d64cb0d3eb5f
ohad.r <oredlich@mobileye.com>
2023-11-17 12:38:19 +0200

    use cases

--- src/runner.ts
+++ src/runner.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 const debug = require('debug')('ajv-sandbox');
 
 import disabledFleetSchema from "../configurations/validation_schemas/Disabled_Fleet.json";
@@ -14,6 +15,21 @@ async function run() {
     ajv.addSchema(enbledFleetSchema, "enbledFleetSchema");
     ajv.addSchema(ohadsSchema, "ohadsSchema");
 
+    const enabler = {
+        "enable_a" : false
+    }
+    let isValid = ajv.validate("enbledFleetSchema", {});
+    debug(isValid);
+    expect(isValid).to.equal(false);
+
+    isValid = ajv.validate("enbledFleetSchema", enabler);       //the schema expects enable_a:"false"
+    debug(isValid);
+    expect(isValid).to.equal(true);
+
+    enabler.enable_a = true
+    isValid = ajv.validate("enbledFleetSchema", enabler);
+    debug(isValid);
+    expect(isValid).to.equal(false);
 
 }
 
